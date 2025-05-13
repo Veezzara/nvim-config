0f0f82963a4dbdccc337d565b2b56896ff2e476e
Max Zhabin <veezzara@gmail.com>
2025-05-13 18:48:18 +0400

    added telescope remaps

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -1,5 +1,7 @@
 local remapper = require('custom.remapper')
+local builtin = require('telescope.builtin')
 
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 remapper.add_remap("n", "<leader>w", ":w<CR>", "Save file", { "File Management" })
 remapper.add_remap("n", "<leader>q", ":q<CR>", "Quit Neovim", { "File Management" })
 remapper.add_remap("n", "<leader>f", ":Telescope find_files<CR>", "Find files", { "Search" })
@@ -14,3 +16,11 @@ remapper.add_remap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', 'Sign
 remapper.add_remap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', 'Rename', { 'LSP' })
 remapper.add_remap({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', 'Format', { 'LSP' })
 remapper.add_remap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', 'Code actions', { 'LSP' })
+
+remapper.add_remap('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+remapper.add_remap('n', '<leader>fg', builtin.git_files, { desc = 'Telescope git files' })
+remapper.add_remap('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
+remapper.add_remap('n', '<leader>lg', builtin.live_grep, { desc = 'Telescope live grep' })
+remapper.add_remap('n', '<leader>fs', builtin.lsp_workspace_symbols, { desc = 'Telescope workspace symbols' })
