113fa5d6b82a613dc60cf491da8754b16b3d1e27
Жабин Максим Евгеньевич <maksim.zhabin@rt.ru>
2025-10-07 19:03:52 +0400

    cleanup

--- lua.legacy/after/colors.lua
+++
@@ -1,6 +0,0 @@
-function SetScheme(scheme)
-	scheme = scheme or "vscode"
-	vim.cmd.colorscheme(scheme)
-end
-
-SetScheme()
--- lua.legacy/after/init.lua
+++
@@ -1,4 +0,0 @@
-require("after.colors")
-require("after.treesitter")
-require("after.fugitive")
-require("after.lsp")
