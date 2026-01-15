e3dd1a17bb076241b64edad07469598e8b317c07
Max Zhabin <veezzara@gmail.com>
2026-01-16 00:31:59 +0400

    set up basic autocomplete

--- init.lua
+++ init.lua
@@ -10,3 +10,14 @@ vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format)
 
 require('config.lazy')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end
+})
+
+vim.cmd('set completeopt+=noselect')
