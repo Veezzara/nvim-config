f7ae8d3c08ae693e7b7dd5a803f956746ddd7929
Max Zhabin <veezzara@gmail.com>
2026-01-27 15:59:36 +0400

    add format fallback

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,9 @@
 return {
 	'stevearc/conform.nvim',
 	opts = {
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
 		formatters_by_ft = {
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			typescript = { "prettierd", "prettier", stop_after_first = true },
